38d05f3fa586952f1b8a7f7ab9d90c9ed4149193
quarkusio <no-reply@quarkus.io>
2024-10-23 23:08:19 +0000

    Initial commit

---
+++ src/native-test/java/com/vmlc/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.vmlc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
